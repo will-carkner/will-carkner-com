be89411952d62a18a6b0d5a34aa4a6d83f970369
ChickenTaxi <willcarkner2006@gmail.com>
2024-05-23 20:21:00 +0100

    did things

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,4 +14,8 @@ export default defineConfig({
       },
     }),
   ],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 })
