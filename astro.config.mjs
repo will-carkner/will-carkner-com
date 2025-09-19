643992e9eb0e449fd373038e2ec727c85ec50df5
ChickenTaxi <willcarkner2006@gmail.com>
2025-09-19 13:06:27 +0100

    hopefully

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/server'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
